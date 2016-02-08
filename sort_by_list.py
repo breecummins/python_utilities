1822e5e37ea54b3a8ad6e454876c23d93f82340f
Bree Cummins <breecummins@gmail.com>
2016-02-08 11:42:51 -0700

    first commit

---
+++ sort_by_list.py
@@ -0,0 +1,8 @@
+def sort_by_list(X,Y):
+	# sort Y by the sorted order of X
+    newX, newY = [], []
+    for (x,y) in sorted(zip(X,Y):
+        newX.append(x)
+        newY.append(y)
+    return newX,newY
+
