786def94b781add5fb919e159769bfe586b6e0fa
Bree Cummins <breecummins@gmail.com>
2016-03-30 15:10:06 -0600

    added gitignore

--- sort_by_list.py
+++ sort_by_list.py
@@ -1,7 +1,7 @@
-def sort_by_list(X,Y):
+def sort_by_list(X,Y,reverse):
 	# sort Y by the sorted order of X
     newX, newY = [], []
-    for (x,y) in sorted(zip(X,Y):
+    for (x,y) in sorted(zip(X,Y),reverse=reverse):
         newX.append(x)
         newY.append(y)
     return newX,newY
